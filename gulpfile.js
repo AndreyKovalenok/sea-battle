55d53d5e953525b871532d887f94496da3e0b233
Andrey <a.kovalenok679@gmail.com>
2020-06-16 12:03:23 +0200

    Настройка проекта

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var htmlmin = require("gulp-htmlmin");
+var del = require("del");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp.src("src/scss/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("html", function() {
+  return gulp.src("src/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      collapseInlineTagWhitespace: true
+    }))
+    .pipe(gulp.dest("build"))
+});
+
+gulp.task("javascript", function() {
+  return gulp.src("src/js/script.js")
+    .pipe(rename("script.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js"))
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "src/fonts/**/*.{woff,woff2}",
+    "src/img/**",
+    "src/js/**",
+    "src/*.ico"
+    ], {
+    base: "src"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("src/scss/**/*.scss", gulp.series("css"));
+  gulp.watch("src/js/*.js", gulp.series("javascript"));
+  gulp.watch("src/img/{icon-*,htmlacademy}.svg", gulp.series("html", "refresh"));
+  gulp.watch("src/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "html",
+  "javascript"
+));
+gulp.task("start", gulp.series("build", "server"));
